8ac0f0710aa796bfbc49f7d3d59cf371c6950394
waniya farooq <waniyafarooq18@gmail.com>
2026-07-15 16:49:37 +0500

    Add home view and URL configuration; create index template

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+# views main fuctions banani han jo url 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hey I am a django ")
+# we are suppose to return a html template
