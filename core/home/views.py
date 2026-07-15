ea509d3706f8d14035ebdd9d4ae8ae551decd03b
waniya farooq <waniyafarooq18@gmail.com>
2026-07-15 18:31:32 +0500

    Add contact and about pages; update home view to render template with context

--- core/home/views.py
+++ core/home/views.py
@@ -5,5 +5,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hey I am a django ")
+    peoples = [
+    { 'name':'Waniya Farooq' , 'age':22},
+    { 'name':'Nadia Farooq' , 'age':52},
+    { 'name':'Muazan Farooq' , 'age':15},
+    { 'name':'Fakiha Farooq' , 'age':22}
+    ]
+    return render(request,"home/index.html",context={'peoples':peoples})
+   # return HttpResponse("Hey I am a django ")
 # we are suppose to return a html template
+
+def contact(request):
+    return render(request,"home/contact.html")
+
+def about(request):
+    return render(request,"home/about.html")
+
