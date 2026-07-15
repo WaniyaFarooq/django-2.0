b6399b8f0a0011d9be1e4970f6bc6e564a4e4c80
waniya farooq <waniyafarooq18@gmail.com>
2026-07-15 20:13:07 +0500

    Add initial migration for Student and product models

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,16 @@
+from itertools import product
+
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True,blank=True)
+    file = models.FileField()
+    
+class product(models.Model):
+    pass
+
