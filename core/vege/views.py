ddb266a31445b3a1cbfe5708c27d9f50787f7e7e
waniya farooq <waniyafarooq18@gmail.com>
2026-07-16 20:45:39 +0500

    Fix input field names in recipe form and update view logic to handle image uploads

--- core/vege/views.py
+++ core/vege/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def receipes(request):
     if request.method == "POST":
         data =request.POST
         rn = data.get("receipe_name")
         rd = data.get("receipe_description")
+        ri = request.FILES.get("receipe_image")
         print(rn)
         print(rd)
+        print(ri)
+        Receipe.objects.create(
+        receipe_name =rn,                 
+        receipe_description =rd,
+        receipe_image =ri)
     return render(request,"receipes/receipes.html")
 
 
