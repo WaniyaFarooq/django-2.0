552076577b276d3ae895ee2f2eb2e07f25510c76
waniya farooq <waniyafarooq18@gmail.com>
2026-07-23 01:29:12 +0500

    Implement custom user model and user manager; update settings to use custom user model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+from django.contrib.auth import get_user_model
+from core.accounts.manager import UserManager
+User = get_user_model()
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=100,unique=True)
+    user_bio  = models.CharField(max_length=50)
+    user_profile_image = models.ImageField(upload_to="profile")
+    
+    USERNAME_FIELD = 'phone_number' 
+    REQUIRED_FIELDS = []
+    objects = UserManager()
--- core/vege/views.py
+++ core/vege/views.py
@@ -7,6 +7,9 @@ from django.contrib import messages
 from django.contrib.auth import authenticate ,login , logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # login to maintain session
 
 # Create your views here.
